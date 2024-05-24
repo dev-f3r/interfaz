fe51fc9f30eda3dd2073af4e4be72679ef20411f
devfercastro <devfercastro@gmail.com>
2024-05-24 01:57:42 +0000

    Creación de las listas con botones para modales

--- js/UI/UIhelpers.js
+++ js/UI/UIhelpers.js
@@ -1,5 +1,6 @@
 import { ELEMENTOS } from "./inicializador.js";
 import { BotonModal } from "./UImodels.js";
+import { colecciones } from "../colecciones/main.js";
 
 /**
  * ? Muestra u oculta el contenedor de direccionales arriba y abajo.
@@ -15,7 +16,6 @@ export function mostrar_direccionales_arriba_abajo() {
 /**
  * ? Crea una lista con los botones a partir de una lista con nombres de objetos.
  * ? Estos botones pueden estar mostrados u ocultos.
- * @param {string[]} lista - La lista con nombres de objetos.
  * @param {Object.<string, Object>} coleccion - La colección en la que se basa la lista de nombres.
  * @param {string} propiedad - La propiedad de los objetos de la colección donde esta el nombre del icono.
  * @param {number} maximo - El máximo de botones visibles.
@@ -44,3 +44,81 @@ export function armar_lista_botones_modal(
         });
     });
 }
+
+/**
+ * Contiene las listas de botones para cada modal
+ */
+const botones_modal = {
+    avatares: {
+        normales: armar_lista_botones_modal(
+            colecciones.personajes.avatares,
+            "portada",
+            MAXIMO_BOTONES_MODAL,
+            "ico"
+        ),
+        especial: new BotonModal({
+            id: "nuevopj_btn",
+            mostrar: true,
+            ruta_icono: "img/nuevopjico.png",
+        }),
+    },
+    // TODO: Implementar lo siguiente
+    // * Botones para modal armas:
+    armas_marciales: {
+        normales: armar_lista_botones_modal(
+            colecciones.armas.armas_marciales,
+            "icono",
+            MAXIMO_BOTONES_MODAL
+        ),
+        especial: new BotonModal({
+            id: "salvajes_btn",
+            mostrar: true,
+            ruta_icono: "img/salvajes.png",
+            // funcionClick: () => {
+            //     ocultar_modales();
+            //     mostrar_modal(modales.armas_naturales);
+            // },
+        }),
+    },
+    armas_naturales: {
+        normales: armar_lista_botones_modal(
+            colecciones.armas.armas_naturales,
+            "icono",
+            MAXIMO_BOTONES_MODAL
+        ),
+        especial: new BotonModal({
+            id: "marciales_btn",
+            clases: ["item-modal"],
+            mostrar: true,
+            ruta_icono: "img/marciales.png",
+            // funcionClick: () => {
+            //     ocultar_modales;
+            //     mostrar_modal(modales.armas_marciales);
+            // },
+        }),
+    },
+    esbirros: {
+        normales: armar_lista_botones_modal(
+            colecciones.personajes.esbirros,
+            "portada",
+            MAXIMO_BOTONES_MODAL,
+            "ico"
+        ),
+        especial: new BotonModal({
+            id: "nuevoesbirro_btn",
+            mostrar: true,
+            ruta_icono: "img/nuevopjico.png",
+        }),
+    },
+    equipos: {
+        normales: armar_lista_botones_modal(
+            colecciones.equipos,
+            "icono",
+            MAXIMO_BOTONES_MODAL
+        ),
+        // TODO: El boton especial debe reestaurar el slot de equipamiento seleccionado.
+        especial: new BotonModal({
+            mostrar: true,
+        }),
+    },
+};
